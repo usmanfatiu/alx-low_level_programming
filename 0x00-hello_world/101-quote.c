1b1c4cabf69b19009097136578c71749c66b5ee2
usmanfatiu <fatiuusman@gmail.com>
2023-08-15 19:02:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unixstd.h>
+/**
+ * main - A c program that prints a lineto a standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
