48228c92a0842b269b19c02c54b61e46958f7aa3
usmanfatiu <fatiuusman@gmail.com>
2023-08-17 09:43:41 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - programs that print number below ten starting from zero
+* Return: always 0
+*/
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
