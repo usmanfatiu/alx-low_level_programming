93efc6f0d6b9e767b97dea81553a4fde7d457cb7
usmanfatiu <fatiuusman@gmail.com>
2023-08-17 10:18:47 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - program that writes letter in reverse
+* return: always 0
+*/
+int main(void)
+{
+char i;
+for (i = 'z' ; i >= 'a' ; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
