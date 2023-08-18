8c90a5f2d4d8e142c26bd85f3d3bce152f93eba5
usmanfatiu <fatiuusman@gmail.com>
2023-08-18 06:07:54 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
-* main - program that print all the possible combination of number
-* Return: always 0
+*main - program that print all the possible combination of number
+*Return: always 0
 */
 int main(void)
 {
 int i;
-for (i = 0 ; i < 9 ; i++)	
+for (i = 0 ; i < 10 ; i++)
 {
 putchar(i + '0');
+if (i != 9)
+{
 putchar(',');
 putchar(' ');
 }
-putchar('9');
+}
 return (0);
 }
