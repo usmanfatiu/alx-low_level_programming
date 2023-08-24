1a685fd3cdb1d5ad5539049c48ad212f1f18bdc9
usmanfatiu <fatiuusman@gmail.com>
2023-08-23 17:45:02 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,7 @@ while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
 str[index - 1] == ';' ||
 str[index - 1] == '.' ||
